84d95538604dd0011b319a5b7213bfac61c33bc4
noman081 <abdullahalnoman642@gmail.com>
2022-03-10 15:32:28 +0600

    Adding product in UI

--- js/app.js
+++ js/app.js
@@ -9,3 +9,21 @@ const useConfirm = () => {
     console.log(giveDecision);
     if (giveDecision) alert('Our web adress is: ' + location.href);
 }
+
+//add product 
+const addProduct = () => {
+    const productNameValue = document.getElementById('product-name');
+    const productPriceValue = document.getElementById('product-price');
+    const productName = productNameValue.value;
+    const productPrice = productPriceValue.value;
+    productNameValue.value = '';
+    productPriceValue.value = '';
+    if (!productName || !productPrice) return;
+    const ul = document.getElementById('products');
+    const li = document.createElement('li');
+    li.innerHTML = `
+    ${productName} ----> ${productPrice}
+    `;
+    ul.appendChild(li);
+
+}
